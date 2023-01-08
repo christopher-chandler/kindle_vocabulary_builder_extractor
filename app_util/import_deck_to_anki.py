17fc42ef494a9d4401ca6cd0bb76399443085194
Christopher chandler <christopher.chandler@outlook.de>
2023-01-08 10:21:52 +0100

    update

--- app_util/import_deck_to_anki.py
+++
@@ -1,7 +0,0 @@
-# Stanard
-import subprocess
-
-def import_deck(name: str) -> None:
-    filename = f'/results/{name}.apkg'
-    app = "/Applications/Anki 2.1.54.app"
-    subprocess.run(['open', "-a",app, filename])
